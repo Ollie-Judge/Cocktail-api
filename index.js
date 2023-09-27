8ec2d3ca5c52c30d51338788494aa6eb83765dc8
Ollie-Judge <ollie.judge@hotmail.com>
2023-09-27 16:57:03 +0100

    added a comment of the random get request

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+/*
+app.get("/searchRandom", (req, res) => {
+    try {
+    const response = await axios.get("www.thecocktaildb.com/api/json/v1/1/random.php");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});*/
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
