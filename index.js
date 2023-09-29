f13e86e9a81ceade821395ccc418a7dac9afa46d
Ollie-Judge <ollie.judge@hotmail.com>
2023-09-29 11:01:28 +0100

    got the output onto the webpage

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+const API_URL = "www.thecocktaildb.com/api/json/v1/1";
 
-/*
-app.get("/searchRandom", (req, res) => {
-    try {
-    const response = await axios.get("www.thecocktaildb.com/api/json/v1/1/random.php");
-    const result = response.data;
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "http://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const result = response.data.drinks;
+    console.log(result);
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
       error: error.message,
     });
+  }
+});
+
+/*
+app.post("/", async (req, res) => {
+  const cocktailName = req.body.cocktailName;
+  try {
+    const result = await axios.get(API_URL + "/search.php?s=" + cocktailName);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
   }
 });*/
 
